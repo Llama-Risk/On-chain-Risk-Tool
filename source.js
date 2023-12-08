a1d8f21b04fe178d2500540b9347e39a97c55af5
crook06 <112490805+crook06@users.noreply.github.com>
2023-12-09 00:05:57 +0100

    Update source.js

--- source.js
+++ source.js
@@ -1,76 +1,109 @@
-const axios = require('axios');
-
-// Define the base API URL for Prisma Monitor and the Railway API
-const PRISMA_API_URL = 'https://api.prismamonitor.com/feeds-docs'; // 
+console.log("script beginning")
+const PRISMA_API_URL = 'https://api.prismamonitor.com'; // 
 const RAILWAY_API = 'https://backend-production-a412.up.railway.app'; // Rik's backend
 
-// Function to fetch troves under a specific collateral ratio
-async function fetchTrovesUnderCR(chain, crThreshold) {
-    const endpoint = `${RAILWAY_API}/v1/trove/${chain}/troves?crThreshold=${crThreshold}`; // example - by using backend, Rik correct me here
+const apiResponse = await Functions.makeHttpRequest({
+  url: RAILWAY_API,
+  method: "GET"
+})
 
-    try {
-        const response = await axios.get(endpoint);
-        const troves = response.data.troves;
-        const totalVaR = troves.reduce((sum, trove) => sum + trove.collateralUSD, 0);
-        const troveCount = troves.length;
+// the amount at 2% impact is the following
+// calculated using regression using log
+const limit_from_impact = apiResponse.data.result.limit_from_impact
+console.log(`2% impact:: ${limit_from_impact}`)
 
-        console.log('Total Protocol VaR:', totalVaR, 'USD');
-        console.log('Number of Troves under', crThreshold, '% CR:', troveCount);
+// volatility
+const average_parkinson = apiResponse.data.result.volatility.average_parkinson
+const latest_ema = apiResponse.data.result.volatility.latest_ema
 
-        return troves;
-    } catch (error) {
-        console.error('Error fetching troves:', error);
-        return [];
+
+async function getStabilityPoolSize() {
+  try {
+    const url = `${PRISMA_API_URL}/v1/mkusd/ethereum/holders`;
+    const response = await Functions.makeHttpRequest({
+        url: url,
+        method: "GET"
+    })
+    const data = response.data;
+
+    for (const holder of data.holders) {
+      if (holder.label === 'Stability Pool') {
+        return holder.value;
+      }
     }
+
+    return null;
+  } catch (error) {
+    throw new Error(`Failed to retrieve stability pool value: ${error.message}`);
+  }
 }
 
-// Function to calculate VaR (Value at Risk) for each collateral pool
-async function calculatePoolVaR(chain, crThreshold, pools) {
-    const troves = await fetchTrovesUnderCR(chain, crThreshold);
-    const totalCollateral = troves.reduce((sum, trove) => sum + trove.collateralUSD, 0);
-    const troveCount = troves.length;
+const stabilityPoolSize = await getStabilityPoolSize()
+console.log(`stabilityPoolSize:: ${stabilityPoolSize}`)
 
-    console.log('Total VaR:', totalCollateral, 'USD');
-    console.log('Number of Troves under 150% CR:', troveCount);
-
-    const poolVaRs = {};
-
-    pools.forEach(pool => {
-        const poolTroves = troves.filter(trove => trove.pool === pool);
-        const poolVaR = poolTroves.reduce((sum, trove) => sum + trove.collateralUSD, 0);
-        poolVaRs[pool] = poolVaR;
-    });
-
-    return poolVaRs;
+async function getMKUSDCirculatingSupply() {
+  const url = `${PRISMA_API_URL}/v1/mkusd/ethereum/general`;
+  try {
+    const response = await Functions.makeHttpRequest({
+        url: url,
+        method: "GET"
+    })
+    const data = response.data;
+    return data.info.supply;
+  } catch (error) {
+    throw new Error(`Error fetching supply data: ${error.message}`);
+  }
 }
 
-// Function to get trade amount for a 2% price impact
-async function getTradeAmountForPriceImpact(chain, collateral) {
-    const endpoint = `https://api.prismamonitor.com/v1/collateral/ethereum${collateral}/impact`;
+const MKUSDCirculatingSupply = await getMKUSDCirculatingSupply()
+console.log(`MKUSDCirculatingSupply ${MKUSDCirculatingSupply}`)
 
-    try {
-        const response = await axios.get(endpoint);
-        const impacts = response.data.impact;
-        const impactData = impacts.find(imp => imp.impact === 0.02);
-        return impactData ? impactData.amount : 0;
-    } catch (error) {
-        console.error('Error fetching trade amount for price impact:', error);
-        return 0;
+async function getStabilityPoolSizeShare() {
+  try {
+    const stabilityPoolSize = await getStabilityPoolSize();
+    const circulatingSupply = await getMKUSDCirculatingSupply();
+
+    if (circulatingSupply === 0) {
+      throw new Error('Circulating supply is zero. Cannot divide by zero.');
     }
+
+    const stabilityPoolSizeShare = stabilityPoolSize / circulatingSupply;
+    return stabilityPoolSizeShare;
+  } catch (error) {
+    // throw new Error(`Error calculating stability pool size share: ${error.message}`);
+    return apiResponse.data.result.stability_pool_share
+  }
 }
 
-// Example usage
-const chain = 'ethereum';
-const crThreshold = 150;
-const pools = ['wstETH', 'rETH', 'cbETH', 'sfrxETH'];
+const stabilityPoolSizeShare = await getStabilityPoolSizeShare()
+console.log(`stabilityPoolSizeShare :: ${stabilityPoolSizeShare}`)
 
-calculatePoolVaR(chain, crThreshold, pools).then(poolVaRs => {
-    console.log('VaR for each pool:', poolVaRs);
+async function getValueAtRiskAndRiskyTroves(ratio=1.5) {
+  const url = `${PRISMA_API_URL}/v1/trove/ethereum/0xbf6883a03fd2fcfa1b9fc588ad6193b3c3178f8f/troves?items=10000&page=1&order_by=last_update&desc=true`;
 
-    // Calculate trade amount for 2% price impact for each pool
-    pools.forEach(pool => {
-        getTradeAmountForPriceImpact(chain, pool).then(tradeAmount => {
-            console.log(`Trade amount for 2% price impact in ${pool}:`, tradeAmount);
-        });
-    });
-});
+  try {
+    const response = await Functions.makeHttpRequest({
+        url: url,
+        method: "GET"
+    })
+    response.data; // Accessing response.data instead of response.json()
+
+    const openTroves = response.data.troves.filter(trove => trove.status === 'Open' && trove.collateral_ratio < ratio);
+    const sumCollateralUSD = openTroves.reduce((sum, trove) => sum + trove.collateral_usd, 0);
+
+    return {
+      "valueAtRisk": sumCollateralUSD,
+      "openTrovesLength": openTroves.length,
+    };
+  } catch (error) {
+    // throw new Error(`Error during API request: ${error.message}`);
+    return {
+      "valueAtRisk": apiResponse.data.result.value_at_risk,
+      "openTrovesLength": apiResponse.data.result.troves_at_risk
+    }
+  }
+}
+
+const valueAtRiskAndRiskyTroves = await getValueAtRiskAndRiskyTroves()
+console.log(`valueAtRisk:: ${valueAtRiskAndRiskyTroves.valueAtRisk}`)
+console.log(`troves at risk:: ${valueAtRiskAndRiskyTroves.openTrovesLength}`)
